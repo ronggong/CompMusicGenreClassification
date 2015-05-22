4f3c6a611dc999545c71087e082d37d72c47cd02
jingjusweetness <jingjusweetness@gmail.com>
2015-05-22 14:17:06 +0200

    fix
    change path and set equal amount of songs for each genre

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import sys
+import os, sys
 sys.path.append('/Users/gong/Documents/Library/gaia-master/src/bindings/pygaia/scripts/classification')
-sys.path.append('../src')
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 import recordingIDfetcher as fetcher
 import jingjuRecordingIDreader as reader
@@ -32,10 +32,10 @@ groundtruthDict = {}
 # ----- write groundtruth and filelist
 ii = 1
 genre = 'Jingju'
-length = len(jingjuRecordingIDs)
+lengthOfJingjuRecordingIDs = len(jingjuRecordingIDs)
 for recordingID in jingjuRecordingIDs:
     # jsonDumper.dumpFeaturesIntoJson(recordingID, outputFolder, genre)
-    print 'dumping low level features ', ii, ' into json file of total ', length 
+    print 'dumping low level features ', ii, ' into json file of total ', lengthOfJingjuRecordingIDs 
     
     filelistJson[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.json'
     filelistYaml[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.sig'
@@ -44,49 +44,113 @@ for recordingID in jingjuRecordingIDs:
 
 
 ii = 1
+jj = 1 # dumping sucessfully index
+loopedRecordingIDs = [] # for 
 genre = 'Carnatic'
-length = len(carnaticRecordingIDs[:500])
-for recordingID in carnaticRecordingIDs[:500]:
-    rFlag = jsonDumper.dumpFeaturesIntoJson(recordingID, outputFolder, genre)
-    if rFlag == True:
-        print 'dumping low level features ', ii, ' into json file of total ', length 
-        recordingID = recordingID.encode('ascii')
-        filelistJson[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.json'
-        filelistYaml[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.sig'
-        groundtruthDict[recordingID] = genre
-    else:
-        print 'dumping low level features ', ii, 'failed of total ', length
-    ii += 1
+length = len(carnaticRecordingIDs)
+for recordingID in carnaticRecordingIDs:
+    if recordingID not in loopedRecordingIDs:
+        rFlag = jsonDumper.dumpFeaturesIntoJson(recordingID, outputFolder, genre)
+        if rFlag == True:
+            print 'dumping low level features ', ii, ' into json file of total ', length 
+            recordingID = recordingID.encode('ascii')
+            filelistJson[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.json'
+            filelistYaml[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.sig'
+            groundtruthDict[recordingID] = genre
+        
+            if jj >= lengthOfJingjuRecordingIDs:
+                print 'stop collecting carnatic music feature, we have the same amont of carnatic as ', lengthOfJingjuRecordingIDs
+                break
+            print 'successfully dumping ', jj, ' into json file' 
+            jj += 1
+        else:
+            print 'dumping low level features ', ii, 'failed of total ', length
+        
+        loopedRecordingIDs.append(recordingID)
+        ii += 1
 
 ii = 1
+jj = 1
 genre = 'Hindustani'
-length = len(hindustaniRecordingIDs[:500])
-for recordingID in hindustaniRecordingIDs[:500]:
-    rFlag = jsonDumper.dumpFeaturesIntoJson(recordingID, outputFolder, genre)
-    if rFlag == True:
-        print 'dumping low level features ', ii, ' into json file of total ', length 
-        recordingID = recordingID.encode('ascii')
-        filelistJson[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.json'
-        filelistYaml[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.sig'
-        groundtruthDict[recordingID] = genre
-    else:
-        print 'dumping low level features ', ii, 'failed of total ', length
-    ii += 1
+loopedRecordingIDs = []
+length = len(hindustaniRecordingIDs)
+for recordingID in hindustaniRecordingIDs:
+    if recordingID not in loopedRecordingIDs:
+        rFlag = jsonDumper.dumpFeaturesIntoJson(recordingID, outputFolder, genre)
+        if rFlag == True:
+            print 'dumping low level features ', ii, ' into json file of total ', length 
+            recordingID = recordingID.encode('ascii')
+            filelistJson[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.json'
+            filelistYaml[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.sig'
+            groundtruthDict[recordingID] = genre
+        
+            if jj >= lengthOfJingjuRecordingIDs:
+                print 'stop collecting hindustani music feature, we have the same amont of hindustani music as ', lengthOfJingjuRecordingIDs
+                break
+            print 'successfully dumping ', jj, ' into json file' 
+            jj += 1
+        else:
+            print 'dumping low level features ', ii, 'failed of total ', length
+            
+        loopedRecordingIDs.append(recordingID)
+        ii += 1
 
 ii = 1
+jj = 1
 genre = 'Makam'
-length = len(makamRecordingIDs[:500])
-for recordingID in makamRecordingIDs[:500]:
-    rFlag = jsonDumper.dumpFeaturesIntoJson(recordingID, outputFolder, genre)
-    if rFlag == True:
-        print 'dumping low level features ', ii, ' into json file of total ', length 
-        recordingID = recordingID.encode('ascii')
-        filelistJson[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.json'
-        filelistYaml[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.sig'
-        groundtruthDict[recordingID] = genre
+loopedRecordingIDs = []
+length = len(makamRecordingIDs)
+for recordingID in makamRecordingIDs:
+    if recordingID not in loopedRecordingIDs:
+        rFlag = jsonDumper.dumpFeaturesIntoJson(recordingID, outputFolder, genre)
+        if rFlag == True and (recordingID not in loopedRecordingIDs):
+            print 'dumping low level features ', ii, ' into json file of total ', length 
+            recordingID = recordingID.encode('ascii')
+            filelistJson[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.json'
+            filelistYaml[recordingID] = outputFolder + '/' + genre +'/' + recordingID +'.sig'
+            groundtruthDict[recordingID] = genre
+        
+            if jj >= lengthOfJingjuRecordingIDs:
+                print 'stop collecting makam music feature, we have the same amont of makam music as ', lengthOfJingjuRecordingIDs
+                break
+            print 'successfully dumping ', jj, ' into json file' 
+            jj += 1
+        else:
+            print 'dumping low level features ', ii, 'failed of total ', length
+        
+        loopedRecordingIDs.append(recordingID)
+        ii += 1
+
+# -----check the song amont of each genre
+jingjuLen = 1
+carnaticLen = 1
+hindustaniLen = 1
+makamLen = 1
+for key, value in groundtruthDict.iteritems():
+    if value == 'Jingju':
+        jingjuLen += 1
+    if value == 'Carnatic':
+        carnaticLen += 1
+    if value == 'Hindustani':
+        hindustaniLen += 1
+    if value == 'Makam':
+        makamLen += 1
+
+def amontCheck(genre, length, jingjuLen):
+    if length != jingjuLen:
+        print genre, length, ' is != ', jingjuLen
+        return False
     else:
-        print 'dumping low level features ', ii, 'failed of total ', length
-    ii += 1
+        print genre, length, ' is = ', jingjuLen
+        return True
+        
+rFlag0 = amontCheck('Carnatic', carnaticLen, jingjuLen)
+rFlag1 = amontCheck('Hindustani', hindustaniLen, jingjuLen)
+rFlag2 = amontCheck('Makam', makamLen, jingjuLen)
+
+if not rFlag0 or not rFlag1 or not rFlag2:
+    print 'classification interrupted, because of non-equal amont of songs.'
+    sys.exit()
 
 # -----write groundtruth
 groundtruth = gtMaker.groundtruthMaker('compMusicGenreClassification', groundtruthDict) # write ground
